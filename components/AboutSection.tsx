5549eacd89b6fc49343c2d8c1f47be05ee8bd923
Fellipe Juncal <fjuncal7@gmail.com>
2025-04-04 18:59:03 -0300

    Creating hero-photo image

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -76,11 +76,11 @@ const AboutSection = () => {
               })}
             </div>
             <Image
-              src="/hero-image.png"
+              src="/hero-image-man2.png"
               alt=""
               width={325}
               height={325}
-              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
+              className=" hidden mt-10 md:block md:relative md:bottom-4 md:left-32 md:z-0"
             />
           </div>
         </div>
